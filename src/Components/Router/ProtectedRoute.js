68d208f2a0ced66fd4d341fa9e7fba8e02f38a8d
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-09-30 21:47:46 +0300

    remove input from ProtectedRoute function

--- src/Components/Router/ProtectedRoute.js
+++ src/Components/Router/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
     const isAuthenticated = localStorage.getItem("authToken");
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />;
