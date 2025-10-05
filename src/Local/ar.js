3786e6a619318a7d0c092bc78a538ee2530fabcc
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-10-05 15:54:30 +0300

    init localization

---
+++ src/Local/ar.js
@@ -0,0 +1,12 @@
+const ar = 
+{
+    "lang" : "ar",
+    "langName" : "العربية",
+    "langCode" : "ar",
+    "langIcon" : "🇸🇦`",
+    "langDirection" : "rtl",
+    "signIn" : "تسجيل الدخول",
+
+}
+
+export default ar
