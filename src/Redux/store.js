6042e1b04d560825a9eb6bb8b1dfd344ee45f2c2
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-10-05 12:20:05 +0300

    make wishlist reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { langSlice } from "./lang.slice.js"
 import { themeSlice } from "./theme.slice.js"
+import { wishlistSlice } from "./wishlist.slice.js"
 
 export const myStroe = configureStore(
     {
         reducer:
         {
             langReducer : langSlice.reducer,
-            themeReducer : themeSlice.reducer
+            themeReducer : themeSlice.reducer,
+            wishlistReducer : wishlistSlice.reducer
         }
     }
 )
---
+++ src/Redux/wishlist.slice.js
@@ -0,0 +1,39 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const wishlistSlice = createSlice({
+    name: 'wishlist',
+    initialState: {
+        items: [],
+    },
+    reducers: {
+        addToWishlist: (state, action) => {
+            const id = action.payload;
+            // Only add if not already in wishlist
+            if (!state.items.includes(id)) {
+                state.items.push(id);
+            }
+        },
+        removeFromWishlist: (state, action) => {
+            const id = action.payload;
+            state.items = state.items.filter(itemId => itemId !== id);
+        },
+        toggleWishlist: (state, action) => {
+            const id = action.payload;
+            const index = state.items.indexOf(id);
+
+            if (index > -1) {
+                // Remove if exists
+                state.items.splice(index, 1);
+            } else {
+                // Add if doesn't exist
+                state.items.push(id);
+            }
+        },
+        clearWishlist: (state) => {
+            state.items = [];
+        },
+    },
+});
+
+export const { addToWishlist, removeFromWishlist, toggleWishlist, clearWishlist } = wishlistSlice.actions;
