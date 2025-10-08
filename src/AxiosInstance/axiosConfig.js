44495417f39eab658ab6ea44955727a9023df524
Mohamed wagdi <74010408+muuhammeed@users.noreply.github.com>
2025-10-08 17:03:05 +0300

    update responsive

--- src/AxiosInstance/axiosConfig.js
+++ src/AxiosInstance/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://3.84.61.66:3000",
+  baseURL: "http://3.84.61.66:4000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
